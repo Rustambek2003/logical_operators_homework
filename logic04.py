28b1cc66e009f8fc8c27f7a9bb06b66152d1bce4
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-10-17 23:05:39 +0500

    4

--- logic04.py
+++ logic04.py
@@ -7,4 +7,10 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
+    answer = 0
+    if a % 2 == 0 and b % 2 == 0:
+        answer = "Each of the numbers 'a' and 'b' is even"
+    else:
+        answer = "Each of the numbers 'a' and 'b' is not even"
+    return answer
+print(main(44,13))
